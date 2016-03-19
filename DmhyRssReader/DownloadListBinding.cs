3001b7ac5854b40ba94f346fe79162a4ad04e24a
Meng Hang <chocolate.menghang@gmail.com>
2016-03-19 16:39:45 +0800

    Add data binding class

---
+++ DmhyRssReader/DownloadListBinding.cs
@@ -0,0 +1,87 @@
+﻿using System.ComponentModel;
+
+namespace DmhyRssReader
+{
+    class DownloadListBinding : INotifyPropertyChanged
+    {
+        private RssListBinding rss;
+        public RssListBinding RSS
+        {
+            get
+            {
+                return this.rss;
+            }
+            set
+            {
+                this.rss = value;
+                this.OnPropertyChanged("RSS");
+            }
+        }
+
+        private string title;
+        public string Title
+        {
+            get
+            {
+                return this.title;
+            }
+            set
+            {
+                this.title = value;
+                this.OnPropertyChanged("Title");
+            }
+        }
+
+        private string updateTime;
+        public string Updatetime
+        {
+            get
+            {
+                return this.updateTime;
+            }
+            set
+            {
+                this.updateTime = value;
+                this.OnPropertyChanged("UpdateTime");
+            }
+        }
+
+        private string magnetLink;
+        public string Magnetlink
+        {
+            get
+            {
+                return this.magnetLink;
+            }
+            set
+            {
+                this.magnetLink = value;
+                this.OnPropertyChanged("MagnetLink");
+            }
+        }
+
+        private bool downloaded;
+        public bool Downloaded
+        {
+            get
+            {
+                return this.downloaded;
+            }
+            set
+            {
+                this.downloaded = value;
+                this.OnPropertyChanged("Downloaded");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ DmhyRssReader/RssListBinding.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace DmhyRssReader
+{
+    public class RssListBinding : INotifyPropertyChanged
+    {
+        private bool seleted;
+        public bool Seleted
+        {
+            get
+            {
+                return this.seleted;
+            }
+            set
+            {
+                this.seleted = value;
+                this.OnPropertyChanged("Seleted");
+            }
+        }
+
+        private string name;
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+                this.OnPropertyChanged("Name");
+            }
+        }
+
+        private string url;
+        public string URL
+        {
+            get
+            {
+                return this.url;
+            }
+            set
+            {
+                this.url = value;
+                this.OnPropertyChanged("URL");
+            }
+        }
+
+        private DateTime updateTimeValue;
+        public DateTime UpdateTimeValue
+        {
+            get
+            {
+                return this.updateTimeValue;
+            }
+            set
+            {
+                this.updateTimeValue = value;
+                this.OnPropertyChanged("UpdateTime");
+            }
+        }
+        public string UpdateTime
+        {
+            get
+            {
+                return this.updateTimeValue.ToString("f");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
